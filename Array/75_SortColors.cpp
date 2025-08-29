d8bf456ed5b2091d3993ecee7b9159b1c25ba7e5
Taksh DIyora <takshdiyora40@gmail.com>
2025-08-29 12:18:44 +0530

    Added Solution of Sort Colors (LeetCode 75)

---
+++ Array/75_SortColors.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, mid = 0;
+        int right = nums.size()-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[mid], nums[left]);
+                left++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums[mid], nums[right]);
+                right--;
+            }
+        }
+    }
+};
