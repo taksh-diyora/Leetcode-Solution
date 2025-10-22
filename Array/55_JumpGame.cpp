29acda7107f85ae36d25543fa542357b8bb49a55
Taksh DIyora <takshdiyora40@gmail.com>
2025-10-22 10:10:18 +0530

    Added Solution of Jump Game (LeetCode 55)

---
+++ Array/55_JumpGame.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(i>maxReach) return false;
+            maxReach = max(maxReach, i+nums[i]);
+            if(maxReach >= n-1) return true;
+        }
+
+        return false;
+    }
+};
