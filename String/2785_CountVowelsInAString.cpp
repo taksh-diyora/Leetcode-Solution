ee328c9201a0edc878158e2b9e7e8450bc1d1e81
Taksh DIyora <takshdiyora40@gmail.com>
2025-11-02 22:01:33 +0530

    Added Solution of Count Vowels in a String (LeetCode 2785)

---
+++ String/2785_CountVowelsInAString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isVowel(char c){
+        return string("AEIOUaeiou").find(c) != string::npos;
+    }
+    string sortVowels(string s) {
+        vector<char> vowels;
+        for(char c: s){
+            if(isVowel(c)){
+                vowels.push_back(c);
+            }
+        }
+
+        sort(vowels.begin(), vowels.end());
+
+        int v_index = 0;
+        for(int i=0; i<s.size(); i++){
+            if(isVowel(s[i])){
+                s[i] = vowels[v_index++];
+            }
+        }
+
+        return s;
+    }
+};
