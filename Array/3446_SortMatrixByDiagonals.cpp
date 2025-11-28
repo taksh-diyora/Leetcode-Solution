854ae0a3e1a7fb98733e00b0289ecee385b9908f
Taksh DIyora <takshdiyora40@gmail.com>
2025-11-28 10:13:09 +0530

    Added Solution of Sort Matrix by Diagonals (LeetCode 3446)

--- Array/3446_SortMatrixByDiagonals.cpp
+++ Array/3446_SortMatrixByDiagonals.cpp
@@ -1,14 +1,32 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> diags(2*n - 1);
 
-vector<vector<int>> sortDiagonal(vector<vector<int>>& matrix){
-    int n = matrix.size();
-    vector<vector<int>> sortM(n, vector<int>(n));
-    int top = 0;
-    int bottom = n-1;
-    int left = 0;
-    int right = n-1;
-    
-}
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int id = n - 1 + (j - i);
+                diags[id].push_back(grid[i][j]);
+            }
+        }
+
+        for(int id = 0; id < 2*n - 1; id++){
+            if(id <= n-1)
+                sort(diags[id].begin(), diags[id].end(), greater<int>());
+            else
+                sort(diags[id].begin(), diags[id].end());
+        }
+
+        vector<int> index(2*n - 1, 0);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int id = n - 1 + (j - i);
+                grid[i][j] = diags[id][index[id]++];
+            }
+        }
+
+        return grid;
+    }
+};
